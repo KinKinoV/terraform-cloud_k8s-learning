c13203b0e19f8122191f2dc99f07545437a86f89
KinKinoV <mr.kolbasa.ask@gmail.com>
2024-10-25 16:09:00 +0300

    Added OIDC providder url and hosted zone NS for ease of configuration

--- terraform/eks/outputs.tf
+++ terraform/eks/outputs.tf
@@ -10,6 +10,11 @@ output "cluster_endpoint" {
   value       = module.eks.cluster_endpoint
 }
 
+output "cluster_oidc_provider" {
+  description = "URL of the cluster's OIDC provider"
+  value       = module.eks.oidc_provider
+}
+
 output "cluster_security_group_id" {
   description = "Security group ids attached to the cluster control plane"
   value       = module.eks.cluster_security_group_id
@@ -23,4 +28,9 @@ output "region" {
 output "cluster_name" {
   description = "Kubernetes Cluster Name"
   value       = module.eks.cluster_name
+}
+
+output "hosted_zone_nameservers" {
+  description = "List of Name Servers used in Hosted Zone"
+  value       = module.eks_hosted-zone.route53_zone_name_servers
 }
