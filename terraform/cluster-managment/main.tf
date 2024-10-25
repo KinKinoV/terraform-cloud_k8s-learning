8f7b7c2d8cb7864989f66aa83b7ed0bec9a3ee5f
KinKinoV <mr.kolbasa.ask@gmail.com>
2024-10-25 16:11:57 +0300

    Added Helm provider and configured it

--- terraform/cluster-managment/main.tf
+++ terraform/cluster-managment/main.tf
@@ -22,3 +22,21 @@ provider "kubernetes" {
     ]
   }
 }
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args = [
+        "eks",
+        "get-token",
+        "--cluster-name",
+        data.aws_eks_cluster.this.name
+      ]
+    }
+  }
+}
