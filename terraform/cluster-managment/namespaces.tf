f42e7be73d88716082b6682ab0a82c819be633db
KinKinoV <mr.kolbasa.ask@gmail.com>
2024-10-25 16:12:32 +0300

    Created separate namespaces for cert-manager and test application

--- terraform/cluster-managment/namespaces.tf
+++ terraform/cluster-managment/namespaces.tf
@@ -1,5 +1,11 @@
-resource "kubernetes_namespace" "test_namespace" {
+resource "kubernetes_namespace" "cert-manager" {
   metadata {
-    name = "test-namespace"
+    name = "cert-manager"
+  }
+}
+
+resource "kubernetes_namespace" "hello-app" {
+  metadata {
+    name = "hello-app"
   }
 }
