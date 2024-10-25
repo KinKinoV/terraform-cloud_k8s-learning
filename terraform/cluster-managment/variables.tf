1b75d48455943b9717f5fd5362f63f9e464fd4fc
KinKinoV <mr.kolbasa.ask@gmail.com>
2024-10-25 16:11:23 +0300

    Added required variable for the cluster's deployments configs

--- terraform/cluster-managment/variables.tf
+++ terraform/cluster-managment/variables.tf
@@ -8,3 +8,13 @@ variable "eks_name" {
   description = "EKS cluster name in AWS"
   type        = string
 }
+
+variable "oidc_provider" {
+  description = "URL of the cluster's OIDC provider"
+  type        = string
+}
+
+variable "clusterIssuer_email" {
+  description = "E-mail to use with ClusterIssuer from cert-manager for ACME"
+  type        = string
+}
