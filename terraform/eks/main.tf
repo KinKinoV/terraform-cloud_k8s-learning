e7a8c9be4061219765ad7463f37f0608e1202180
KinKinoV <mr.kolbasa.ask@gmail.com>
2024-10-29 12:48:24 +0200

    Removed OIDC audience configurations

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -58,24 +58,22 @@ module "irsa-ebs-csi" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version = "5.39.0"
 
-  create_role                   = true
-  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
-  provider_url                  = module.eks.oidc_provider
-  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
-  oidc_fully_qualified_audiences = [ "sts.awsamazon.com" ]
-  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+  create_role                    = true
+  role_name                      = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
+  provider_url                   = module.eks.oidc_provider
+  role_policy_arns               = [data.aws_iam_policy.ebs_csi_policy.arn]
+  oidc_fully_qualified_subjects  = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
 }
 
 module "irsa-vpc-cni" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version = "5.39.0"
 
-  create_role                   = true
-  role_name                     = "AmazonEKS_VPC_CNI_Role-${module.eks.cluster_name}"
-  provider_url                  = module.eks.oidc_provider
-  role_policy_arns              = [data.aws_iam_policy.vpc_cni_policy.arn]
-  oidc_fully_qualified_audiences = [ "sts.awsamazon.com" ]
-  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:aws-node"]
+  create_role                    = true
+  role_name                      = "AmazonEKS_VPC_CNI_Role-${module.eks.cluster_name}"
+  provider_url                   = module.eks.oidc_provider
+  role_policy_arns               = [data.aws_iam_policy.vpc_cni_policy.arn]
+  oidc_fully_qualified_subjects  = ["system:serviceaccount:kube-system:aws-node"]
 }
 
 module "eks" {
